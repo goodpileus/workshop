6bc246065f389d81e5a0670bf36d55c5280e6cdc
Aidan Quinlan <aidankquinlan@gmail.com>
2025-02-12 17:47:29 -0800

    init drab

---
+++ drab/js/scripts.js
@@ -0,0 +1,47 @@
+//randomize children
+$.fn.randomize = function() {
+  this.each(function(){
+      let childrenArray = $(this).children().toArray();
+      let shuffledChildrenArray = fisherYatesShuffle(childrenArray);
+      $(shuffledChildrenArray).detach().appendTo(this);
+  });
+
+  return this;
+  
+  function fisherYatesShuffle(array){
+      let currentIndex = array.length, temporaryValue, randomIndex;
+      while (currentIndex !== 0) {
+          randomIndex = Math.floor(Math.random() * currentIndex);
+          currentIndex -= 1;
+          temporaryValue = array[currentIndex];
+          array[currentIndex] = array[randomIndex];
+          array[randomIndex] = temporaryValue;
+      }
+      return array;
+  }
+}
+
+$('.drab-slab').randomize();
+
+var x = 0;
+var intervalID = setInterval(function () {
+    $('.drab-slab').randomize();
+
+    if (++x === 4) {
+        window.clearInterval(intervalID);
+    }
+}, 100);
+
+$( ".drab-slab" ).on( "click", function() {
+    window.clearInterval(intervalID);
+
+    var x = 0;
+    var intervalID = setInterval(function () {
+        $('.drab-slab').randomize();
+        
+        if (++x === 4) {
+            window.clearInterval(intervalID);
+        }
+    }, 100);
+
+});
